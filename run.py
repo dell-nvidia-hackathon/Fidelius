740c5d22c68a96d67eb7bba306c85e928d4c7ecd
Pranav Satish <117537359+prnv0@users.noreply.github.com>
2024-10-03 05:29:11 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import subprocess
 import threading
 import os
+
 # Function to install Python dependencies
 def install_python_dependencies():
     if os.path.exists('./requirements.txt'):
@@ -17,6 +18,14 @@ def install_nextjs_dependencies():
         subprocess.run(["npm.cmd", "install"], cwd=client_path)
     else:
         print("No 'package.json' found, skipping Next.js dependency installation.")
+
+# Function to run model_save.py
+def run_model_save():
+    try:
+        print("Running model_save.py...")
+        subprocess.run(["python", "./code/server/model_save.py"], check=True)  # Adjust to "python3" if needed
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred while running model_save.py: {e}")
 
 # Function to run the Flask server
 def run_flask():
@@ -38,9 +47,12 @@ if __name__ == '__main__':
     install_python_dependencies()
     install_nextjs_dependencies()
 
-    # Step 2: Start Flask server in a separate thread
+    # Step 2: Run model_save.py
+    run_model_save()
+
+    # Step 3: Start Flask server in a separate thread
     flask_thread = threading.Thread(target=run_flask)
     flask_thread.start()
 
-    # Step 3: Start Next.js frontend in the main thread
+    # Step 4: Start Next.js frontend in the main thread
     run_nextjs()
