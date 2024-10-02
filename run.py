07dc5f5c71d6939d47e240e8daf2fb8de9e26362
Pranav Satish <117537359+prnv0@users.noreply.github.com>
2024-10-03 05:27:18 +0530

    Update run.py

--- run.py
+++ run.py
@@ -3,15 +3,15 @@ import threading
 import os
 # Function to install Python dependencies
 def install_python_dependencies():
-    if os.path.exists('./Server/requirements.txt'):
+    if os.path.exists('./requirements.txt'):
         print("Installing Python dependencies...")
-        subprocess.run(["pip", "install", "-r", "./Server/requirements.txt"])
+        subprocess.run(["pip", "install", "-r", "./requirements.txt"])
     else:
         print("No 'requirements.txt' file found, skipping Python dependency installation.")
 
 # Function to install Next.js dependencies
 def install_nextjs_dependencies():
-    client_path = "./client"
+    client_path = "./code/client"
     if os.path.exists(os.path.join(client_path, 'package.json')):
         print("Installing Next.js dependencies...")
         subprocess.run(["npm.cmd", "install"], cwd=client_path)
@@ -21,13 +21,13 @@ def install_nextjs_dependencies():
 # Function to run the Flask server
 def run_flask():
     try:
-        subprocess.run(["python", "./Server/server.py"], check=True)  # Adjust to "python3" if needed
+        subprocess.run(["python", "./code/Server/server.py"], check=True)  # Adjust to "python3" if needed
     except subprocess.CalledProcessError as e:
         print(f"Error occurred while running Flask: {e}")
 
 # Function to run the Next.js app using "npm run dev"
 def run_nextjs():
-    client_path = "./client"
+    client_path = "./code/client"
     try:
         subprocess.run(["npm.cmd", "run", "dev"], cwd=client_path, check=True)  # Adjust to "npm.cmd" for Windows
     except subprocess.CalledProcessError as e:
